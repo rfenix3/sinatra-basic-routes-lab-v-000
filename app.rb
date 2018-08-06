7d9e56460629583011538f60aed884d1d32c9443
Richard Fenix <richard.fenix@gmail.com>
2018-08-06 22:54:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Richard Phoenix."
+  end
+
+  get '/hometown' do
+    "My hometown is Quezon City."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Everything is awesome!''"
+  end
+
 end
